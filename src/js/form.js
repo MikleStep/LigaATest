4b7ea2309fd053654be5af0c414adcb31e2af68c
Steppanov Mikhail <follout13@gmail.com>
2020-03-14 20:16:46 +0300

    Переключение форм

--- src/js/form.js
+++ src/js/form.js
@@ -5,4 +5,26 @@ export const init = () => {
     "mask": "+7 (999) 999-99-99",
     showMaskOnHover: false
   }).mask(inputsTel);
+
+};
+export const firstValid = () => {
+  var brand = $('#form__input--brand').val();
+  var sphere = $('#form__input--sphere').val();
+  
+
+  if (brand.length != 0 && sphere.length != 0) {
+    $('#first-submit').prop('disabled', false);
+  } else {
+    $('#first-submit').prop('disabled', true);
+  }
+};
+
+export const secondValid = () => {
+  var name = $('#form__input--name').val();
+  var tel = $('#form__input--tel').val();
+  if (name.length != 0 && tel.length == 18) {
+    $('#second-submit').prop('disabled', false);
+  } else {
+    $('#second-submit').prop('disabled', true);
+  }
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,23 @@
-import {init as popupInit} from './popup.js';
+import { init as popupInit} from './popup.js';
 import { init as formInit } from './form.js';
+import { firstValid as firstform } from './form.js';
+import { secondValid as secondform} from './form.js';
 
 $(document).ready(function() {
   popupInit();
   formInit();
+});
+$('.form__first').on('mouseout', function () {
+  firstform();
+})
+$('.form__second').on('mouseout', function () {
+  secondform();
+})
+$('#first-submit').click(function () {
+  $('.form__first').addClass('disable').fadeOut(300);
+  $('.form__second').removeClass('disable').fadeIn(300);
+});
+$('#second-submit').on('click', function () {
+  $('.form__second').addClass('disable').fadeOut(300);
+  $('.form__third').removeClass('disable').fadeIn(300);
 });
