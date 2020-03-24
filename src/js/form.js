1edc8172df959eac67ebbce9e1090060b96abb96
Steppanov Mikhail <follout13@gmail.com>
2020-03-24 18:47:01 +0300

    Валидация форм

--- src/js/form.js
+++ src/js/form.js
@@ -2,29 +2,19 @@ export const init = () => {
   var inputsTel = document.querySelectorAll('input[type="tel"]');
 
   Inputmask({
-    "mask": "+7 (999) 999-99-99",
+    "mask": "+380 (999) 999-99-99",
     showMaskOnHover: false
   }).mask(inputsTel);
 
 };
-export const firstValid = () => {
-  var brand = $('#form__input--brand').val();
-  var sphere = $('#form__input--sphere').val();
-  
 
-  if (brand.length != 0 && sphere.length != 0) {
-    $('#first-submit').prop('disabled', false);
-  } else {
-    $('#first-submit').prop('disabled', true);
-  }
-};
+// export const secondValid = () => {
+//   var name = $('#form__input--name').val();
+//   var tel = $('#form__input--tel').val();
+//   if (name.length != 0 && tel.length > 17 && tel.indexOf('_') < 0) {
+//     $('#second-submit').prop('disabled', false);
+//   } else {
+//     $('#second-submit').prop('disabled', true);
+//   }
+// };
 
-export const secondValid = () => {
-  var name = $('#form__input--name').val();
-  var tel = $('#form__input--tel').val();
-  if (name.length != 0 && tel.length > 17 && tel.indexOf('_') < 0) {
-    $('#second-submit').prop('disabled', false);
-  } else {
-    $('#second-submit').prop('disabled', true);
-  }
-};
--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,79 @@
 import { init as popupInit} from './popup.js';
 import { init as formInit } from './form.js';
-import { firstValid as firstform } from './form.js';
-import { secondValid as secondform} from './form.js';
 
 $(document).ready(function() {
   popupInit();
   formInit();
 });
-$('.form__first').on('keyup', function () {
-  firstform();
-})
-$('.form__second').on('keyup', function () {
-  secondform();
-})
-$('#first-submit').click(function () {
-  $('.form__first').fadeOut(300);
-  $('.form__second').delay(300).fadeIn(300);
+
+$('#first-submit').on('click', function firstStepError() {
+  var brand = $('#form__input--brand').val();
+  var sphere = $('#form__input--sphere').val();
+  var error = $(".form__error--first");
+  var err_text = "";
+  if (sphere == '' && brand == '') {
+    err_text = "<p>Введите навзание вашего бренда для его проверки</p><p>Начните вводить сферу деятельности из предложенных вариантов выберите наиболее подходящую</p>";
+    error.html(err_text);
+    error.fadeIn(300);
+    $('.form__wrapper').addClass('active');
+    return false;
+  }
+  if (brand == '' && sphere != '') {
+    err_text = "<p>Введите навзание вашего бренда для его проверки</p>";
+    error.html(err_text);
+    error.fadeIn(300);
+    $('.form__wrapper').addClass('active');
+    return false;
+  }
+  if (brand != '' && sphere == '') {
+    err_text = "<p>Начните вводить сферу деятельности из предложенных вариантов выберите наиболее подходящую</p>";
+    error.html(err_text);
+    error.fadeIn(300);
+    $('.form__wrapper').addClass('active');
+    return false;
+  }
+  else{
+    error.fadeOut(300);
+    $('.form__wrapper').removeClass('active');
+    $('.form__first').fadeOut(300);
+    $('.form__second').delay(300).fadeIn(300);
+    return true
+  }
+  
 });
-$('#second-submit').on('click', function () {
-  $('.form__second').fadeOut(300);
-  $('.form__third').delay(300).fadeIn(300);
+
+$('#second-submit').on('click', function secondStepError() {
+  var name = $('#form__input--name').val();
+  var tel = $('#form__input--tel').val();
+  var error = $(".form__error--second");
+  var err_text = "";
+  if (name == '' && tel == '') {
+    err_text = "<p>Введите ваше имя, чтобы мы знали как к вам обращаться</p><p>Введите ваш телефон, чтобы мы с вами связаться</p>";
+    error.html(err_text);
+    error.fadeIn(300);
+    $('.form__wrapper').addClass('active');
+    console.log('Сука работай!');
+    return false;
+  }
+  if (name == '') {
+    err_text = "<p>Введите ваше имя, чтобы мы знали как к вам обращаться</p>";
+    error.html(err_text);
+    error.fadeIn(300);
+    $('.form__wrapper').addClass('active');
+    return false;
+  }
+  if (tel == '' || tel.indexOf('_') > 0) {
+    err_text = "<p>Введите ваш телефон, чтобы мы с вами связаться</p>";
+    error.html(err_text);
+    error.fadeIn(300);
+    $('.form__wrapper').addClass('active');
+    return false;
+  } else {
+    error.fadeOut(300);
+    $('.form__wrapper').removeClass('active');
+    $('.form__second').fadeOut(300);
+    $('.form__third').delay(300).fadeIn(300);
+    return true
+  }
+
 });
